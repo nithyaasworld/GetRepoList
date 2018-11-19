c061dcf5e510d7b08392a43c7a81faac13b4560c
Nithya Sen <nithyaasworld@gmail.com>
2018-11-19 17:42:32 +0530

    1.Removed external trigger to start the program 2. Added function to fetch repolist based on UserInput

--- getRepoList.js
+++ getRepoList.js
@@ -1,19 +1,22 @@
-var http = require('http');
 const axios = require('axios');
 
-http.createServer(function(req,res){
+var gitUserName = process.stdin;
+gitUserName.setEncoding('utf-8');
 
-	axios.get('https://api.github.com/users/arocki7/repos')
+console.log("Please input Git UserName:");
+
+//Function to get Repo Names of Entered Username
+
+gitUserName.on('data',function(data){
+				
+	axios.get(`https://api.github.com/users/${data.trim()}/repos`)
 	.then(result => {
-		console.log("Printing Repo Names:");
+		console.log(`\nPrinting Repo List of the User: ${data.trim()}\n`);
 		for (var i=0; i<result["data"].length; i++ ){
-		console.log(result["data"][i]["full_name"]);
+		var repoName = result["data"][i]["full_name"];
+		var repoNameTrimmed = repoName.replace(`${data.trim()}/`,"");
+		console.log(repoNameTrimmed);
 		}
 	})
-	.catch(err => console.log(err.toString()));
-
-	
-    //res.setHeader('Content-Type', 'application/json');
-	//res.write("success");
-	res.end();
-}).listen(8080);
+	.catch(err => console.log("No Such User Found"));
+})
