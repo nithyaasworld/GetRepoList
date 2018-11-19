1574aed24c09ed4ce440b2ae83fce5335f2aa3d4
Nithya Sen <nithyaasworld@gmail.com>
2018-11-19 15:23:59 +0530

    js file - code alignment done

--- getRepoList.js
+++ getRepoList.js
@@ -1,23 +1,19 @@
 var http = require('http');
 const axios = require('axios');
 
-
-
 http.createServer(function(req,res){
 
-		
-			axios.get('https://api.github.com/users/arocki7/repos')
-				.then(result => {
+	axios.get('https://api.github.com/users/arocki7/repos')
+	.then(result => {
+		console.log("Printing Repo Names:");
+		for (var i=0; i<result["data"].length; i++ ){
+		console.log(result["data"][i]["full_name"]);
+		}
+	})
+	.catch(err => console.log(err.toString()));
 
-				console.log("Printing Repo Names:");
-				for (var i=0; i<result["data"].length; i++ ){
-				console.log(result["data"][i]["full_name"]);
-				}});
-			 //.catch(err => console.log(err.toString()));
-
-		
-
-		    //res.setHeader('Content-Type', 'application/json');
-			//res.write("success");
-			res.end();
+	
+    //res.setHeader('Content-Type', 'application/json');
+	//res.write("success");
+	res.end();
 }).listen(8080);
